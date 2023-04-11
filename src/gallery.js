577644a54210c9708868cf88588674a2ff407712
rafallakomiec <114759971+rafallakomiec@users.noreply.github.com>
2023-04-11 14:04:21 +0200

    markup complete

---
+++ src/gallery.js
@@ -0,0 +1,5 @@
+import 'modern-normalize/modern-normalize.css';
+import { Notify } from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+const axios = require('axios').default;
+const debounce = require('lodash.debounce');
